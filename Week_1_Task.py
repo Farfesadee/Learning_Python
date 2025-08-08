3ef00f4b1cc30d1cba4f8bd696a972fc24d8fde0
Odushile Omodolapo <odushileomodolapo1995@gmail.com>
2025-08-08 12:09:07 +0100

    Week 1 Task added

---
+++ Week_1_Task.py
@@ -0,0 +1,51 @@
+# Task 1
+print("        Task 1\n")
+first_name = "Omodolapo"
+last_name = "Odushile"
+middle_name = "Odunayo"
+uni_attended = "University of Ilorin, Ilorin, Nigeria"
+local_government = "Sagamu"
+favourite_food = "Rice and Beans"
+print(f"Name: {first_name} {last_name} {middle_name}\nUniversity: {uni_attended}\nLocal Government: {local_government}\nFavourtite Nigerian Food: {favourite_food}")
+
+print ()
+
+# Task 2
+print("       Task 2\n")
+name = "Odushile Omodolapo Odunayo"
+state_of_origin = "Ogun"
+print("Name:", name)
+print("State of Origin:", state_of_origin)
+
+print()
+
+# Task 3
+print("      Task3\n")
+print("Below is a simple timetable for a day in a Nigerian Secondary School.")
+print("       Time\t                  Subjects\n")
+print("8:00 AM - 9:00 AM\t         Mathematics\n")
+print("9:00 AM - 10:00 AM\t        English Language\n")
+print("10:00 AM - 11:00 AM\t        Biology\n")
+print("11:00 AM - 12:00 PM\t        Break Time\n")
+print("12:00 PM - 1:00 PM\t        Physics\n")
+print("1:00 PM - 2:00 PM\t         Animal Husbandry\n")
+print("2:00 PM - 3:00 PM\t         Fellowship and Closing")
+
+print()
+
+# Task 4
+print("       Task 4\n")
+name = "Odushile Omodolapo Odunayo"
+class_name = "S.S.S 2"
+best_subject = "mathematics"
+print(f"My name is {name}, I am in {class_name} and my best subject is {best_subject}.")
+
+
+print()
+
+# Task 5
+print("       Task 5\n")
+poem = """A land of vibrant hues,
+Where unity finds its voice,
+A future right awaits."""
+print(poem)
